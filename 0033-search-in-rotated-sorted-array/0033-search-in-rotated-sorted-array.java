d8c9c36016ac9a7f2e3f3ba36860c93e0221feb6
poojasingh2429 <poojasinghrajput2429@gmail.com>
2025-06-30 22:50:50 +0530

    Time: 0 ms (100%), Space: 42.2 MB (39.99%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,63 +1,33 @@
 public class Solution {
     public int search(int[] nums, int target) {
-        int pivot = findPivot(nums);
-
-        // If pivot is -1, the array is not rotated
-        if (pivot == -1) {
-            return binarySearch(nums, target, 0, nums.length - 1);
-        }
-
-        // If pivot is the target
-        if (nums[pivot] == target) {
-            return pivot;
-        }
-
-        // If target is in the left sorted part
-        if (target >= nums[0]) {
-            return binarySearch(nums, target, 0, pivot - 1);
-        }
-
-        // If target is in the right sorted part
-        return binarySearch(nums, target, pivot + 1, nums.length - 1);
-    }
-
-    // Binary search helper function
-    int binarySearch(int[] arr, int target, int start, int end) {
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
-                start = mid + 1;
-            } else {
-                return mid;
-            }
-        }
-        return -1;
-    }
-
-    // Find pivot: index where rotation happens
-    int findPivot(int[] arr) {
         int start = 0;
-        int end = arr.length - 1;
+        int end = nums.length - 1;
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
-            // 4 cases
-            if (mid < end && arr[mid] > arr[mid + 1]) {
+            if (nums[mid] == target) {
                 return mid;
             }
-            if (mid > start && arr[mid] < arr[mid - 1]) {
-                return mid - 1;
+
+            // Check if left half is sorted
+            if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;  // target in left part
+                } else {
+                    start = mid + 1;  // target in right part
+                }
             }
-            if (arr[mid] <= arr[start]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+            // Otherwise, right half is sorted
+            else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;  // target in right part
+                } else {
+                    end = mid - 1;  // target in left part
+                }
             }
         }
+
         return -1;
     }
 }
